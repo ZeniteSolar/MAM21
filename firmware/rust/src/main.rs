dbf95e28a21eabe36f679a04d44927a8fd371b71
João Antônio Cardoso <joao.maker@gmail.com>
2022-06-17 21:08:10 -0300

    firmware: Hello World!

---
+++ firmware/rust/src/main.rs
@@ -0,0 +1,54 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use cortex_m::delay::Delay;
+use cortex_m_rt::entry; // The runtime
+use stm32_hal2::{
+    self,
+    clocks::{Clocks},
+    gpio::{Pin, PinMode, Port},
+    pac,
+};
+
+use defmt_rtt as _;
+
+// This marks the entrypoint of our application. The cortex_m_rt creates some
+// startup code before this, but we don't need to worry about this
+#[entry]
+fn main() -> ! {
+    // Get handles to the hardware objects. These functions can only be called
+    // once, so that the borrowchecker can ensure you don't reconfigure
+    // something by accident.
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let _dp = pac::Peripherals::take().unwrap();
+
+    defmt::println!("Hello, world!");
+
+    let clock_cfg = Clocks::default();
+
+    // Write the clock configuration to the MCU. If you wish, you can modify `clocks` above
+    // in accordance with [its docs](https://docs.rs/stm32-hal2/0.2.0/stm32_hal2/clocks/index.html),
+    // and the `clock_cfg` example.
+    clock_cfg.setup().unwrap();
+
+    // Setup a delay, based on the Cortex-m systick.
+    let mut delay = Delay::new(cp.SYST, clock_cfg.systick());
+    let mut led = Pin::new(Port::C, 13, PinMode::Output);
+
+    // Now, enjoy the lightshow!
+    loop {
+        defmt::debug!("Our demo is alive");
+        led.set_low();
+        delay.delay_ms(1000_u32);
+        led.set_high();
+        delay.delay_ms(1000_u32);
+    }
+}
+
+// same panicking *behavior* as `panic-probe` but doesn't print a panic message
+// this prevents the panic message being printed *twice* when `defmt::panic` is invoked
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    cortex_m::asm::udf()
+}
